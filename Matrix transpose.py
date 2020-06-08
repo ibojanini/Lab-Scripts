e2284c867e73c5df174b94696bfd31c865140472
ibojanini <49567602+ibojanini@users.noreply.github.com>
2020-06-08 15:20:58 -0700

    Update Matrix transpose.py
    The input is the gene_presence_absence.Rtab obtained using Roary. This code creates a file containing the gene presence (1) and absence (0) for each isolate. The resulting file can be used for GLOOME analysis.

--- Matrix transpose.py
+++ Matrix transpose.py
@@ -3,19 +3,17 @@
 import pandas as pd
 import numpy as np
 
-data=pd.read_csv('CA_nov_gene_presence_absence.Rtab', sep='\t') #sep= lo que separa la data 
-#print(data)
+data=pd.read_csv('CA_nov_gene_presence_absence.Rtab', sep='\t') 
 
 data_transpose=data.transpose()
 data_transpose.to_csv('2data_transpose_csv.csv')
 
 with open ('2data_transpose_csv.csv','r') as gene_presence_absence_fasta:
-    # parse file =ir de linea en linea
     line_list = []
     for ticker, line in enumerate(gene_presence_absence_fasta):
         if ticker>1:
-            line = line.replace(',', '') #to remove commas
-            line_split = line.split('_') #here I am separating by whatever is left from _ from right.
+            line = line.replace(',', '') 
+            line_split = line.split('_') 
             gene_name = '>{0}'.format('_'.join(line_split[:-1]))
             gene_name+='_{0}\n{1}'.format(line_split[-1][:2],line_split[-1][2:])
             line_list.append(gene_name)
